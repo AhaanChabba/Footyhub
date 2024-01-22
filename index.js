e794f1e8387f9691f50a933efb962e626213a247
Antwone <antwone_henry@horacemann.org>
2024-01-22 10:47:21 -0500

    trying to fix my github issue

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ app.get('/teams', (req, res) => {
     res.json(teams);
   });
 
-
 ///gets a team by id
 
 
 app.get('/teams/:id', (req, res) => {
+
+    //looked this functionality up
     const teamId = parseInt(req.params.id);
     const team = teams.find(t => t.id === teamId);
   
