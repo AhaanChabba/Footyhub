232ab649141142de2b59aef2e840e86879fe3082
Ahaan Chabba <ahaan_chabba@horacemann.org>
2024-01-30 19:54:30 -0500

    pushing my db.js code

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ app.get('/teams', (req, res) => {
 });
 
 
-
+//Gets all players
 const getPlayerById = (req, res) => {
 
     const playerId = parseInt(req.body.id);
@@ -27,6 +27,7 @@ const getPlayerById = (req, res) => {
 
 };
 
+//Gets all teams  
 const getTeamById = (req, res) => {
 
     // 
@@ -48,5 +49,50 @@ const getTeamById = (req, res) => {
     }); 
 };
 
+// Gets all countries
+const getCountryById = (req, res) => {
+    const query = `SELECT * FROM Country`;
+
+    db.all(query, [], (error, results) => {
+        if (error) {
+            console.error(error.message);
+            res.status(400).json({ error: error.message });
+            return;
+        }
+
+        if (result) {
+            res.json(result);
+        } else {
+            res.sendStatus(404);
+        }
+    }); 
+};
+
+// Gets all leagues
+const getLeagueById = (req, res) => {
+    const query = `SELECT * FROM League`;
+
+    db.all(query, [], (error, results) => {
+        if (error) {
+            console.error(error.message);
+            res.status(400).json({ error: error.message });
+            return;
+        }
+
+        if (result) {
+            res.json(result);
+        } else {
+            res.sendStatus(404);
+        }
+    }); 
+};
+
 
 /// need to understand the difference between .body and .params
+
+module.exports = {
+    getLeagueById,
+    getCountryById,
+    getTeamById,
+    getPlayerById,
+  };
